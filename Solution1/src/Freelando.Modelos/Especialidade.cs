62cac1729dde63e0cdf7c89a966e51b323ecf5dc
franciscof <franciscof@cetsp.com.br>
2025-01-09 23:59:16 -0300

    Implementa a aula Configurando o projeto

--- Solution1/src/Freelando.Modelos/Especialidade.cs
+++ Solution1/src/Freelando.Modelos/Especialidade.cs
@@ -1,9 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Freelando.Modelo;
+[Table("TB_Especialidades")]
 public class Especialidade
 {
+    [Column("Id_Especialidade")]
     public Guid Id { get; set; }
-    public string? Descricao { get; set; }
 
+    [Column("DS_Especialidade")]
+    public string? Descricao { get; set; }
 }
