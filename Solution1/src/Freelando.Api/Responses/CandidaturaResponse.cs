72f9121314177d75e1536e24ee4de864bb8ef173
franciscof <franciscof@cetsp.com.br>
2025-01-13 23:27:40 -0300

    Insere comentario na classe

--- Solution1/src/Freelando.Api/Responses/CandidaturaResponse.cs
+++ Solution1/src/Freelando.Api/Responses/CandidaturaResponse.cs
@@ -1,5 +1,5 @@
 ﻿using Freelando.Modelo;
 
 namespace Freelando.Api.Responses;
-
+//record
 public record CandidaturaResponse(Guid Id, double? ValorProposto, string? DescricaoProposta, string? DuracaoProposta, string? Status);
